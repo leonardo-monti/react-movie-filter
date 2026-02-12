7373cd19ffd8093c680dd378c5cefb6ae4b5599c
leonardo-monti <monti.leonardo22@gmail.com>
2026-02-12 13:48:13 +0100

    add input

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,25 @@ import movies from './data/Movies'
 
 function App() {
 
+  const [searchGenre, setSearchGenre] = useState("")
+
   return (
     <div>
       <h1>BLOCKBUSTER</h1>
+
       <ul>
         {movies.map((movie) => (
-          <li key={movie.title}>{movie.title}-{movie.genre}
+          <li key={movie.title}>
+            {movie.title} - {movie.genre}
           </li>
         ))}
-
       </ul>
 
+      <input type="text"
+        placeholder="Scrivi un genere"
+        value={searchGenre}
+        onChange={(e) => setSearchGenre(e.target.value)}
+      />
 
     </div>
 
